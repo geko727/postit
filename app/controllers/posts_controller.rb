8bfbabb6732d7a893ec1c4e1ce7aed4d62701163
Carlos Manuel <geko727@gmail,com>
2013-08-14 13:58:49 -0500

    second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,23 @@ class PostsController < ApplicationController
   end
 
   def show
-
+  	@post = Post.find(params[:id])
   end	
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(params.require(:post).permit!)
+
+  	if @post.save
+  		flash[:notice] = "You created a new post!"
+  		redirect_to posts_path
+  	else
+  		#validations
+  		render :new
+  	end
+
+  end
 end
