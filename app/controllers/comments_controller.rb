b36eba241d5fe8a2bb034de2786aad1532e305c8
geko727 <geko727@gmail.com>
2013-08-31 23:37:16 -0500

    Ago31 23:37

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 	before_action :require_user
+	before_action :set_comment, only: [:vote]
+	
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = Comment.new(params.require(:comment).permit(:body))
@@ -13,4 +15,15 @@ class CommentsController < ApplicationController
 			render 'posts/show'
 		end
 	end
+
+	def vote
+		Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+	    flash[:notice] = "Your vote was counted"
+	    redirect_to post_path(params[:post_id])
+	end
+	
+	def set_comment
+      @comment = Comment.find(params[:id])
+  	end  
+
 end
